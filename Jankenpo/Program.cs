2915fc28398edce744bbfb3aa97f8c78f4e973e0
maymai <mmaiafiorini@gmail.com>
2023-02-20 17:08:29 +0000

    Rock paper scissors project

---
+++ Jankenpo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jankenpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
